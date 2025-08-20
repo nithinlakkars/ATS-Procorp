c73611bafbb32f4648a92e28f78d97836e91ffe7
nithin_0201 <lakkarsnithin03@gmail.com>
2025-08-20 20:01:17 +0530

    make chagnes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 let corsOptions;
 if (process.env.NODE_ENV === "production") {
   corsOptions = {
-    origin: process.env.FRONTEND_URL || "https://procorp-ats-frontend.onrender.com",
+    origin: process.env.FRONTEND_URL || "https://procorp-frontend-ats.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
